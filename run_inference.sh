d18596788d17d705173f2fc5e624e1526bfc4460
Yu-Hsuan, Liu <jennyliu.lyh@iir.csie.ncku.edu.tw>
2024-04-10 10:13:39 +0800

    [Feature]Modify inference code for multi-class classification

--- run_inference.sh
+++ run_inference.sh
@@ -1,13 +1,13 @@
 # ############## Cancer Classification ###############
-declare -a CANCER=("BRCA")
-# declare -a CANCER=("BRCA" "LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ")
+# declare -a CANCER=("LUAD LUSC")
+declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
 PARTITION=(1)
 SENSITIVE='{"race": ["white", "black or african american"]}'
 
 for cancer in "${CANCER[@]}";
 do for partition in ${PARTITION[@]};
 do python inference.py --cancer $cancer \
-                  --model_path="./pretrained_weight/" \
+                  --model_path="./models_gender/" \
                   --partition=$partition \
                   --fair_attr="$SENSITIVE" \
                   --task=1 \
@@ -16,55 +16,58 @@ do python inference.py --cancer $cancer \
     done
     done
 
-# declare -a CANCER=("BRCA" "LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ")
-# PARTITION=(1)
-# SENSITIVE='{"race": ["white", "black or african american"]}'
+# declare -a CANCER=("LUAD LUSC")
+declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+PARTITION=(1)
+SENSITIVE='{"race": ["white", "black or african american"]}'
 
-# for cancer in "${CANCER[@]}";
-# do for partition in ${PARTITION[@]};
-# do python inference.py --cancer $cancer \
-#                   --model_path="./models/" \
-#                   --partition=$partition \
-#                   --fair_attr="$SENSITIVE" \
-#                   --task=1 \
-#                   --seed=0 \
-#                   --reweight \
-#                   --device="cuda"
-#     done
-#     done
+for cancer in "${CANCER[@]}";
+do for partition in ${PARTITION[@]};
+do python inference.py --cancer $cancer \
+                  --model_path="./models_gender/" \
+                  --partition=$partition \
+                  --fair_attr="$SENSITIVE" \
+                  --task=1 \
+                  --seed=0 \
+                  --reweight \
+                  --device="cuda"
+    done
+    done
 
-# declare -a CANCER=("BRCA" "LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH")
-# PARTITION=(2)
-# SENSITIVE='{"race": ["white", "black or african american"]}'
+# declare -a CANCER=("LUAD LUSC")
+declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+PARTITION=(2)
+SENSITIVE='{"race": ["white", "black or african american"]}'
 
-# for cancer in "${CANCER[@]}";
-# do for partition in ${PARTITION[@]};
-# do python inference.py --cancer $cancer \
-#                   --model_path="./models/" \
-#                   --partition=$partition \
-#                   --fair_attr="$SENSITIVE" \
-#                   --task=1 \
-#                   --seed=0 \
-#                   --device="cuda"
-#     done
-#     done
+for cancer in "${CANCER[@]}";
+do for partition in ${PARTITION[@]};
+do python inference.py --cancer $cancer \
+                  --model_path="./models_gender/" \
+                  --partition=$partition \
+                  --fair_attr="$SENSITIVE" \
+                  --task=1 \
+                  --seed=0 \
+                  --device="cuda"
+    done
+    done
 
-# declare -a CANCER=("BRCA" "LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH")
-# PARTITION=(2)
-# SENSITIVE='{"race": ["white", "black or african american"]}'
+# declare -a CANCER=("LUAD LUSC")
+declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+PARTITION=(2)
+SENSITIVE='{"race": ["white", "black or african american"]}'
 
-# for cancer in "${CANCER[@]}";
-# do for partition in ${PARTITION[@]};
-# do python inference.py --cancer $cancer \
-#                   --model_path="./models/" \
-#                   --partition=$partition \
-#                   --fair_attr="$SENSITIVE" \
-#                   --task=1 \
-#                   --seed=0 \
-#                   --reweight \
-#                   --device="cuda"
-#     done
-#     done
+for cancer in "${CANCER[@]}";
+do for partition in ${PARTITION[@]};
+do python inference.py --cancer $cancer \
+                  --model_path="./models_gender/" \
+                  --partition=$partition \
+                  --fair_attr="$SENSITIVE" \
+                  --task=1 \
+                  --seed=0 \
+                  --reweight \
+                  --device="cuda"
+    done
+    done
 
 # ############## Tumor Detection ###############
 
--- run_train_gender.sh
+++ run_train_gender.sh
@@ -1,87 +1,91 @@
 ############### Cancer Classification ###############
 
-declare -a CANCER=("LUAD LUSC")
-PARTITION=(1)
-SENSITIVE='{"gender": ["female", "male"]}'
+# declare -a CANCER=("KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+# # declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+# PARTITION=(1)
+# SENSITIVE='{"gender": ["female", "male"]}'
 
-for cancer in "${CANCER[@]}";
-do for partition in ${PARTITION[@]};
-do python main_base.py --cancer $cancer \
-                  --model_path="./models_gender/" \
-                  --partition=$partition \
-                  --fair_attr="$SENSITIVE" \
-                  --task=1 \
-                  --lr=1e-5 \
-                  --dropout=0.3 \
-                  --seed=0 \
-                  --epochs=150 \
-                  --batch_size=16 \
-                  --acc_grad=2 \
-                  --scheduler_step=10 \
-                  --scheduler_gamma=0.9 \
-                  --device="cuda"
-    done
-    done
+# for cancer in "${CANCER[@]}";
+# do for partition in ${PARTITION[@]};
+# do python main_base.py --cancer $cancer \
+#                   --model_path="./models_gender/" \
+#                   --partition=$partition \
+#                   --fair_attr="$SENSITIVE" \
+#                   --task=1 \
+#                   --lr=1e-5 \
+#                   --dropout=0.3 \
+#                   --seed=0 \
+#                   --epochs=150 \
+#                   --batch_size=16 \
+#                   --acc_grad=2 \
+#                   --scheduler_step=10 \
+#                   --scheduler_gamma=0.9 \
+#                   --device="cuda"
+#     done
+#     done
 
 
-declare -a CANCER=("LUAD LUSC")
-PARTITION=(1)
-SENSITIVE='{"gender": ["female", "male"]}'
+# declare -a CANCER=("KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+# # declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+# PARTITION=(1)
+# SENSITIVE='{"gender": ["female", "male"]}'
 
-for cancer in "${CANCER[@]}";
-do for partition in ${PARTITION[@]};
-do python main_base.py --cancer $cancer \
-                  --model_path="./models_gender/" \
-                  --weight_path="" \
-                  --partition=$partition \
-                  --fair_attr="$SENSITIVE" \
-                  --task=1 \
-                  --lr=1e-6 \
-                  --dropout=0.3 \
-                  --seed=0 \
-                  --epochs=150 \
-                  --batch_size=16 \
-                  --acc_grad=2 \
-                  --scheduler_step=10 \
-                  --scheduler_gamma=0.9 \
-                  --fair_lambda=1 \
-                  --constraint="" \
-                  --reweight \
-                  --selection="avgEOpp" \
-                  --device="cuda"
-    done
-    done
+# for cancer in "${CANCER[@]}";
+# do for partition in ${PARTITION[@]};
+# do python main_base.py --cancer $cancer \
+#                   --model_path="./models_gender/" \
+#                   --weight_path="" \
+#                   --partition=$partition \
+#                   --fair_attr="$SENSITIVE" \
+#                   --task=1 \
+#                   --lr=1e-6 \
+#                   --dropout=0.3 \
+#                   --seed=0 \
+#                   --epochs=150 \
+#                   --batch_size=16 \
+#                   --acc_grad=2 \
+#                   --scheduler_step=10 \
+#                   --scheduler_gamma=0.9 \
+#                   --fair_lambda=1 \
+#                   --constraint="" \
+#                   --reweight \
+#                   --selection="avgEOpp" \
+#                   --device="cuda"
+#     done
+#     done
 
-declare -a CANCER=("LUAD LUSC")
+# declare -a CANCER=("KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+# # declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
+# PARTITION=(2)
+# CURR=(0 1 2 3)
+# SENSITIVE='{"gender": ["female", "male"]}'
+
+# for cancer in "${CANCER[@]}";
+# do for partition in ${PARTITION[@]};
+# do for curr in ${CURR[@]};
+# do python main_base.py --cancer $cancer \
+#                   --model_path="./models_gender/" \
+#                   --partition=$partition \
+#                   --curr_fold=$curr \
+#                   --fair_attr="$SENSITIVE" \
+#                   --task=1 \
+#                   --lr=1e-5 \
+#                   --dropout=0.3 \
+#                   --seed=0 \
+#                   --epochs=100 \
+#                   --batch_size=16 \
+#                   --acc_grad=2 \
+#                   --scheduler_step=10 \
+#                   --scheduler_gamma=0.9 \
+#                   --device="cuda"
+# done
+# done
+# done
+
+declare -a CANCER=("GBM LGG")
+# declare -a CANCER=("LUAD LUSC" "KIRP KIRC KICH" "KIRP KIRC" "KIRP KICH" "KIRC KICH" "COAD READ" "GBM LGG")
 PARTITION=(2)
-CURR=(0 1 2 3)
-SENSITIVE='{"gender": ["female", "male"]}'
-
-for cancer in "${CANCER[@]}";
-do for partition in ${PARTITION[@]};
-do for curr in ${CURR[@]};
-do python main_base.py --cancer $cancer \
-                  --model_path="./models_gender/" \
-                  --partition=$partition \
-                  --curr_fold=$curr \
-                  --fair_attr="$SENSITIVE" \
-                  --task=1 \
-                  --lr=1e-5 \
-                  --dropout=0.3 \
-                  --seed=0 \
-                  --epochs=100 \
-                  --batch_size=16 \
-                  --acc_grad=2 \
-                  --scheduler_step=10 \
-                  --scheduler_gamma=0.9 \
-                  --device="cuda"
-done
-done
-done
-
-declare -a CANCER=("LUAD LUSC")
-PARTITION=(2)
-CURR=(0 1 2 3)
+CURR=(1 2 3)
 SENSITIVE='{"gender": ["female", "male"]}'
 
 for cancer in "${CANCER[@]}";
